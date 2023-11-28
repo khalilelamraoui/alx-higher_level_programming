4f3a346e3666b113c2fd7ce9bc758f05aa8f4236
khalilelamraoui <khalilelam16@gmail.com>
2023-11-28 15:58:52 +0100

    fixed pow function

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    if b < 0:
-        a = 1 / a
-        b = -b
-    for _ in range(b):
-        result *= a
-    return result
+    return a ** b
