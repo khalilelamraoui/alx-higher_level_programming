a8a577631f9369865ee752796c44d08697497bf8
khalilelamraoui <khalilelam16@gmail.com>
2023-11-28 15:36:50 +0100

    added task 8 function and main program

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def uppercase(s):
+    uppercase_string = ""
     for char in s:
         ascii_value = ord(char)
         if 97 <= ascii_value <= 122:
             upper_ascii_value = ascii_value - 32
-            print("{}".format(chr(upper_ascii_value)), end='')
+            uppercase_string += chr(upper_ascii_value)
         else:
-            print("{}".format(char), end='')
-    print()
+            uppercase_string += char
+    print(uppercase_string)
