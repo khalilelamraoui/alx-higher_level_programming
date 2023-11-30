91d0b2ca514585309862ae047f19e7d6015e3f31
khalilelamraoui <khalilelam16@gmail.com>
2023-11-30 23:11:33 +0100

    added task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    sum = 0
+    if argc == 1:
+        print("{:d}".format(sum))
+    else:
+        for i in range(1, argc):
+            sum += int(sys.argv[i])
+        print("{:d}".format(sum))
+        
