74349987fc7a17906bb86ffc26f39fa7d2dd4c36
khalilelamraoui <khalilelam16@gmail.com>
2023-12-03 21:45:38 +0100

    fixed tas 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 
 
 def multiple_returns(sentence):
-    if sentence == '':
-        sentence[0] = None
+    if len(sentence) == 0:
+        tup = 0, None
     else:
         tup = len(sentence), sentence[0]
-        return tup
+    return tup
