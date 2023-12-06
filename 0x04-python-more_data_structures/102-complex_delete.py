8de01763251545ecc2d80e654036af9f9c18f8e7
khalilelamraoui <khalilelam16@gmail.com>
2023-12-06 23:11:47 +0100

    fixed advanced task 3

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     if len(a_dictionary) == 0 or a_dictionary is None:
-        return None
+        return a_dictionary
     else:
         for key, val in list(a_dictionary.items()):
             if val == value:
