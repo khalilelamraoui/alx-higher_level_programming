fa0188090b3a7014b4179045ebb70684d3cfc0b4
khalilelamraoui <khalilelam16@gmail.com>
2023-12-06 22:04:32 +0100

    added task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = type(roman_string)
-    romanLength = len(roman_string)
-    if roman_string is None or roman is not str:
+    if type(roman_string) != str or roman_string is None:
         return 0
     roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                         'C': 100, 'D': 500, 'M': 1000}
@@ -10,8 +8,8 @@ def roman_to_int(roman_string):
     for element in roman_string:
         roman_list.append(roman_dictionary[element])
     counter = 0
-    for i in range(romanLength):
-        if i + 1 == romanLength:
+    for i in range(len(roman_string)):
+        if i + 1 == len(roman_string):
             counter += roman_list[i]
         elif roman_list[i] >= roman_list[i + 1]:
             counter += roman_list[i]
