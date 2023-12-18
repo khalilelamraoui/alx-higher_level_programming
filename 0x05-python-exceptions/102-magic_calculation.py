f2192ef0b956c7aac98389160b0332f38557cdbc
khalilelamraoui <khalilelam16@gmail.com>
2023-12-18 18:45:40 +0100

    fixed magic calculation func

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 import dis
+
+
 def magic_calculation(a, b):
-    return 98 + (a ** b)
+    output = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                output += (a ** b) / i
+        except:
+            output = a + b
+            break
+    return output
