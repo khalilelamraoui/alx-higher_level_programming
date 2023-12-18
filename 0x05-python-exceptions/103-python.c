37477ac8bed6715c482a301ef7506ec2c435988a
khalilelamraoui <khalilelam16@gmail.com>
2023-12-18 19:07:30 +0100

    fixed c code

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -7,10 +7,6 @@
 #include<floatobject.h>
 #include<bytesobject.h>
 
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-void print_python_float(PyObject *p);
-
 /**
 * print_python_float - prints some basic info about Python lists
 * @p: PyObject
