ba904465228f77193ccfcb0db5b327b18cb9ad66
khalilelamraoui <khalilelam16@gmail.com>
2023-12-18 18:52:12 +0100

    added function's prototypes

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<Python.h>
+#include<listobject.h>
+#include<floatobject.h>
+#include<bytesobject.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
