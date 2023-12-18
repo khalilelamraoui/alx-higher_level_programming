151c82a1b5d4ae92a5a77fb5cd240511ab99f920
khalilelamraoui <khalilelam16@gmail.com>
2023-12-18 13:55:34 +0100

    added task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    elemtsPrinted = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            elemtsPrinted += 1
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+    print()
+    return elemtsPrinted
