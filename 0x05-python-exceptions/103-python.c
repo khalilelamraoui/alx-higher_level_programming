6740f87d39963b6687ee5919a5b39a3a013ec0fc
khalilelamraoui <khalilelam16@gmail.com>
2023-12-18 18:51:04 +0100

    added function's prototypes

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
 * print_python_list - prints some basic info about Python lists
 * @p: pointer to PyObject
