ad00d34560d87f19e8f0226b6eddc3590482d7ad
khalilelamraoui <khalilelam16@gmail.com>
2023-12-19 12:31:45 +0100

    added task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Initialized Square class"""
+
+
+class Square:
+    """Square class with private attribute"""
+    def __init__(self, size=0):
+        """Initialized Square with size attribute"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
