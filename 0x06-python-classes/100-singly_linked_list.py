ac43e02bcd048e8dabde2a5741e7d6ca0dd79040
khalilelamraoui <khalilelam16@gmail.com>
2023-12-19 13:35:29 +0100

    fixed TypeError problems and checs

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -19,7 +19,8 @@ class Node:
         """data setter"""
         if type(value) is not int:
             raise TypeError("data must be an integer")
-        self.__data = value
+        else:
+            self.__data = value
 
     @property
     def next_node(self):
@@ -31,7 +32,8 @@ class Node:
         """next_node setter"""
         if type(value) is not Node and value is not None:
             raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+        else:
+            self.__next_node = value
 
 
 class SinglyLinkedList:
