d8c93a90061a9eeaf86c8330e3e96e40f2ebed65
khalilelamraoui <khalilelam16@gmail.com>
2023-12-19 15:19:50 +0100

    fixed code

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -5,15 +5,18 @@
 class Square:
     """Square class with private attribute"""
     def __init__(self, size=0, position=(0, 0)):
+        """Initialized Square with size attribute"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Size getter"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Size setter"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -23,10 +26,12 @@ class Square:
 
     @property
     def position(self):
+        """Position getter"""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """Position setter"""
         if not isinstance(value, tuple) or len(value) != 2 or \
                 not all(isinstance(i, int) for i in value) or \
                 any(i < 0 for i in value):
@@ -35,9 +40,11 @@ class Square:
             self.__position = value
 
     def area(self):
+        """Calculation of square's area"""
         return self.__size ** 2
 
     def my_print(self):
+        """Prints a square with #"""
         if self.__size == 0:
             print()
         else:
@@ -47,6 +54,7 @@ class Square:
                 print(" " * self.__position[0] + "#" * self.__size)
 
     def __str__(self):
+        """Returns a string representation of the square"""
         result = []
         if self.__size == 0:
             return ""
