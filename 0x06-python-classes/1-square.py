e9cda226a91787ec342910627450fa946f4ed929
khalilelamraoui <khalilelam16@gmail.com>
2023-12-19 12:07:59 +0100

    added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Initialized Square class"""
+
+
+class Square:
+    """Square class with private attribute"""
+    def __init__(self, size):
+        """Initialized Square with size attribute"""
+        self.__size = size
