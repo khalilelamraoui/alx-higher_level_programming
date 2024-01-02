412631257594adb4eb042f1ff5a316d5db2a97b3
khalilelamraoui <khalilelam16@gmail.com>
2024-01-02 14:40:38 +0100

    added advanced task 2

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that defines a function that multiplies 2 matrices by using the module NumPy
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Function that multiplies 2 matrices by using the module NumPy
+    """
+    return np.matmul(m_a, m_b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/101-lazy_matrix_mul.txt")
