3ca15e4de7ca92b5af65685f48daccaa788b423d
khalilelamraoui <khalilelam16@gmail.com>
2024-01-04 18:02:48 +0100

    added advanced task

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+import sys
+
+
+def is_safe(board, row, col, N):
+    """Method that checks if a queen can be placed on board"""
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, row, N, solutions):
+    """Recursive method that solves the N queens problem"""
+    if row == N:
+        solutions.append([[i, j] for i, row in enumerate(board) for j,
+                          val in enumerate(row) if val == 1])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solve_nqueens_util(board, row + 1, N, solutions)
+            board[row][col] = 0
+
+
+def solve_nqueens(N):
+    """Method that solves the N queens problem"""
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0] * N for _ in range(N)]
+    solutions = []
+
+    solve_nqueens_util(board, 0, N, solutions)
+
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        solve_nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
