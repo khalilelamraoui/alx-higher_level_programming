e2cd1c1107b1d9ec0439ec5421e688aa8e422245
khalilelamraoui <khalilelam16@gmail.com>
2024-01-08 14:29:01 +0100

    added task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that contains a function that
+returns the list of available
+"""
+
+
+def lookup(obj):
+    """
+    returns the list of available
+    attributes and methods of an object
+    """
+    return dir(obj)
