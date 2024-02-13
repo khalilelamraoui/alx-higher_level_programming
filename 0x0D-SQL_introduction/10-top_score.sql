04d65c901845b6cec19312e1e3272662b747cd8b
khalilelamraoui <khalilelam16@gmail.com>
2024-02-13 13:29:29 +0100

    added tasks 9 and 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- listing all records of the table second_table
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- creating a table second_table
+CREATE TABLE IF NOT EXISTS second_table ( id INT, name VARCHAR(256), score INT);
+-- inserting records in the table second_table
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
