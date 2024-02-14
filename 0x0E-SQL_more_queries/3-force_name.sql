7b3e039d288bbfa5db281df1bedbc09e8a148140
khalilelamraoui <khalilelam16@gmail.com>
2024-02-14 13:28:21 +0100

    added 3 more tasks

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- creating table force_name
+CREATE TABLE IF NOT EXISTS force_name ( id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- creating table unique_id
+CREATE TABLE IF NOT EXISTS unique_id ( id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
