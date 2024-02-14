e98a4a227a40d880dd482888d34acb4564b5ba87
khalilelamraoui <khalilelam16@gmail.com>
2024-02-14 14:26:52 +0100

    added 2 advanced tasks

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,3 @@
+-- importing data from hbtn_0d_tvshows_rate
+-- listing all shows by their rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_show_ratings.show_id ORDER BY rating DESC;
