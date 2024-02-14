6d58c254c75c1dca061f14910913e5b7e31c8d1c
khalilelamraoui <khalilelam16@gmail.com>
2024-02-14 13:23:34 +0100

    added 3 tasks

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creating MySQL server user user_0d_1 and granting all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
