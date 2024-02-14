fffd2876d54bc6fc1ae1d314c9d9892fe758fb0c
khalilelamraoui <khalilelam16@gmail.com>
2024-02-14 14:24:32 +0100

    added 2 advanced tasks

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,3 @@
+-- importing data from hbtn_0d_tvshows
+-- displaying all genres not linked to Dexter show
+SELECT g.name FROM tv_genres g WHERE g.name NOT IN (SELECT g.name FROM tv_genres g INNER JOIN tv_show_genres m ON g.id = m.genre_id INNER JOIN tv_shows s ON m.show_id = s.id WHERE s.title = 'Dexter') ORDER BY g.name ASC;
