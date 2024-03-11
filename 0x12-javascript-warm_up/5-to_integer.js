f538f2824f50f5ce7e211d44888e4e4eb6f9b775
khalilelamraoui <khalilelam16@gmail.com>
2024-03-11 19:15:23 +0000

    added tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 const arg1 = Math.floor(Number(process.argv[2]));
-if (isNaN(arg1)) {
-  console.log('Not a number');
-}
-else {
-  console.log(`My number: ${arg1}`);
-}
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
