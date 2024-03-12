a743035502e2ea6988e9f514966bfcce1ddd0a58
khalilelamraoui <khalilelam16@gmail.com>
2024-03-12 15:08:41 +0000

    fixed problems

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const arg1 = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+const num = Math.floor(process.argv[2]);
+if (Number.isInteger(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}
