b836f8ac54fa21216201c007e49c2419f01cf4dd
khalilelamraoui <khalilelam16@gmail.com>
2024-03-27 22:49:36 +0000

    added task 4 5

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send DELETE request and display response body
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
