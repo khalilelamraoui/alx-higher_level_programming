d6a5337b62fe501bda06ec236009e189640a437b
khalilelamraoui <khalilelam16@gmail.com>
2024-03-28 21:01:25 +0000

    added tasks 6-7

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,11 +4,10 @@ fetches https://intranet.hbtn.io/status
 """
 import requests
 
-
-if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    response = requests.get(url)
-    text = response.text
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
     print("Body response:")
     print("\t- type: {}".format(type(text)))
     print("\t- content: {}".format(text))
