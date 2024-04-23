e0b6df930152cd616316424c96b24282a46112b1
khalilelamraoui <khalilelam16@gmail.com>
2024-04-23 19:08:54 +0100

    added tasks 0-1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * This script reads a file and prints its content
+ */
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
