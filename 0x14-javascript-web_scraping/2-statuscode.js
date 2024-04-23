5e2c26f6ff6382ce12b93586484e00ebac2225e5
khalilelamraoui <khalilelam16@gmail.com>
2024-04-23 19:14:44 +0100

    added tasks 2-3

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* 
+ * This script sends a request to a URL 
+ * and prints the status code
+ */
+
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.log(`Usage: ./${process.argv[1]} <URL>`);
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
+  console.log('code:', response.statusCode);
+});
---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* 
+ * This script sends a request to a URL
+ * and prints the title of the Star Wars movie
+ * with the given ID
+ */
+
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.log(`Usage: ./${process.argv[1]} <ID>`);
+  process.exit(1);
+}
+
+const id = process.argv[2];
+
+request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, (err, res) => {
+  if (err) {
+    console.error('Error: ', err.message);
+    process.exit(1);
+  }
+
+  console.log(JSON.parse(res.body).title);
+});
